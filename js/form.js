8ffc723ef25ddf533392ad9ceb141bdfdbed4944
willpizzas <88903897+willpizzas@users.noreply.github.com>
2021-08-15 15:25:55 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -19,7 +19,9 @@ document.querySelector("#submit").addEventListener("click", e => {
 		*ENVIANDO PEDIDO*%0A%0A
 
 		*COMIDA*%0A
+		$  %0A
 		${cliente}%0A
+		$  %0A
 		*BEBIDA*%0A
 		${fecha}%0A
 		*PAGAMENTO*%0A
